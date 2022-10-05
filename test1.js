4ea764d700b3a02ccfae94a5a9c7fdec3fceae2b
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-10-05 00:52:36 +0000

    update answers

---
+++ test1.js
@@ -0,0 +1,23 @@
+const pets = ['Cat', 'Dog', 'Bird', 'Fish', 'Frog', 'Hamster', 'Pig', 'Horse', 'Lion', 'Dragon'];
+// Print all pets
+console.log(pets[0]);
+console.log(pets[1]);
+console.log(pets[2]);
+console.log(pets[3]);
+
+body {
+    font-family: "Times New Roman", Times, serif;
+    font-size: 1rem;
+}
+
+.cat {
+    color: #FFF;
+}
+
+.dog {
+color: #000;
+}
+
+.dragon {
+color: #009933;
+}
